4ba1deaea63e28e38e8005de4db65b3e5f4b553b
Darko <dbindacz@gmail.com>
2016-06-28 17:40:53 +0200

    Added Polygon and Triangle

---
+++ ComputationalGeometry2D/Polygon.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ComparingDoubles;
+using MoreLinq;
+
+namespace ComputationalGeometry2D
+{
+    public class Polygon
+    {
+        public List<Point> Vertices { get; private set; }
+
+        public double MinX() => Vertices.MinBy(p => p.X).X;
+
+        public double MaxX() => Vertices.MaxBy(p => p.X).X;
+
+        public double MinY() => Vertices.MinBy(p => p.Y).Y;
+
+        public double MaxY() => Vertices.MaxBy(p => p.Y).Y;
+
+        public Polygon(IEnumerable<Point> vertices)
+        {
+            Vertices = new List<Point>();
+            AddVertices(vertices);
+        }
+
+        public Polygon(IEnumerable<Point> vertices, int verticesCount)
+        {
+            Vertices = new List<Point>(verticesCount);
+            AddVertices(vertices);
+        }
+
+        private void AddVertices(IEnumerable<Point> vertices)
+        {
+            foreach (Point vertex in vertices)
+                this.Vertices.Add(vertex);
+        }
+
+
+        public LineSegment[] Edges()
+        {
+            LineSegment[] edges = new LineSegment[Vertices.Count];
+            int verticesCountLessTwo = Vertices.Count - 2;
+            for (int i = 0; i <= verticesCountLessTwo; i++)
+                edges[i] = new LineSegment(Vertices[i], Vertices[i + 1]);
+            edges[edges.Length - 1] = new LineSegment(Vertices[Vertices.Count - 1], Vertices[0]);
+            return edges;
+        }
+
+        public bool Contains(Point point) // Point in polygon algorithm.
+        {
+            Line horizontalLine = new Line(0, 1, -point.Y); // Horizontal line passing through the 'point'.
+            LineSegment[] edges = Edges();
+            int leftIntersectionsAmount = 0;
+            int verticesCount = Vertices.Count;
+            Line iEdgeLine = new Line(0, 0, 0);
+            for (int i = 0; i < verticesCount; i++)
+            {
+                LineSegment iEdge = edges[i];
+                if (iEdge.IntersectsWith(horizontalLine))
+                {
+                    iEdgeLine.Reinitialize(iEdge);
+                    Point intersection = horizontalLine.TryIntersection(iEdgeLine);
+                    if (intersection != null) // If iEdgeLine is not horizontal (if lines do not overlap and are not parallel).
+                    {
+                        if (intersection.X.IsLessThanAndNotAlmostEqualTo(point.X) && !intersection.Y.IsAlmostEqualTo(iEdge.MinY))
+                            leftIntersectionsAmount++;
+                    }
+                }
+            }
+            return leftIntersectionsAmount % 2 == 1;
+        }
+    }
+}
---
+++ ComputationalGeometry2D/Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ComparingDoubles;
+
+namespace ComputationalGeometry2D
+{
+    public class Triangle
+    {
+        public Point V1 { get; set; }
+        public Point V2 { get; set; }
+        public Point V3 { get; set; }
+
+        public Triangle(Point v1, Point v2, Point v3)
+        {
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+        }
+
+        public LineSegment[] Edges() =>
+            new[]
+                {
+                    new LineSegment(V1, V2),
+                    new LineSegment(V2, V3),
+                    new LineSegment(V3, V1),
+                };
+        
+
+        private static Point circumcircleCenter = new Point(0.0, 0.0);
+
+        public bool ContainsInCircumcircle(Point point)
+        {
+            double v1XSqrd = V1.X * V1.X;
+            double v1YSqrd = V1.Y * V1.Y;
+            double v2XSqrd = V2.X * V2.X;
+            double v2YSqrd = V2.Y * V2.Y;
+            double v3XSqrd = V3.X * V3.X;
+            double v3YSqrd = V3.Y * V3.Y;
+
+            circumcircleCenter.X = 0.5 * ((v2XSqrd * V3.Y + v2YSqrd * V3.Y - v1XSqrd * V3.Y + v1XSqrd * V2.Y - v1YSqrd * V3.Y + v1YSqrd * V2.Y + V1.Y * v3XSqrd + V1.Y * v3YSqrd - V1.Y * v2XSqrd - V1.Y * v2YSqrd - V2.Y * v3XSqrd - V2.Y * v3YSqrd) / (V1.Y * V3.X - V1.Y * V2.X - V2.Y * V3.X - V3.Y * V1.X + V3.Y * V2.X + V2.Y * V1.X));
+            circumcircleCenter.Y = 0.5 * ((-V1.X * v3XSqrd - V1.X * v3YSqrd + V1.X * v2XSqrd + V1.X * V2.Y + V2.X * v3XSqrd + V2.X * v3YSqrd - v2XSqrd * V3.X - v2YSqrd * V3.X + v1XSqrd * V3.X - v1XSqrd * V2.X + v1YSqrd * V3.X - v1YSqrd * V2.X) / (V1.Y * V3.X - V1.Y * V2.X - V2.Y * V3.X - V3.Y * V1.X + V3.Y * V2.X + V2.Y * V1.X));
+
+            double radious = circumcircleCenter.DistanceFrom(V1);
+
+            return circumcircleCenter.DistanceFrom(point).IsLessThanOrAlmostEqualTo(radious);
+        }
+
+        public bool SharesVertexWith(Triangle other) =>
+            V1.CoordinatesEqual(other.V1) || V1.CoordinatesEqual(other.V2) || V1.CoordinatesEqual(other.V3) ||
+            V2.CoordinatesEqual(other.V1) || V2.CoordinatesEqual(other.V2) || V2.CoordinatesEqual(other.V3) ||
+            V3.CoordinatesEqual(other.V1) || V3.CoordinatesEqual(other.V2) || V3.CoordinatesEqual(other.V3);
+    }
+}
